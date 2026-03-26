c297fa2e20cbc99955c512c54c9147649a244fd7
liupengcheng2012 <liupengcheng38@huawei.com>
2026-03-26 12:13:01 +0000

    init

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# check bishengir-compile and bishengir-opt exist
+curdir="/coursegrader/submit"
+if [ ! -e "$curdir/bishengir-compile" || ! -e "$curdir/bishengir-opt" ]; then
+    echo "please put bishengir-compile and bishengir-opt in $curdir"
+    exit 1
+fi
+
+# add execute permission
+chmod +x $curdir
+export PATH=$curdir:$PATH
+which bishengir-compile
+which bishengir-opt
+
+# run case_judge.py
+python3 /coursegrader/dockerext/case_judge.py
